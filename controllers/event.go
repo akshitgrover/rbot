003906064eade7eeaa232a699ca9dabf4e98b3b6
Akshit Grover <akshit.grover2016@gmail.com>
2018-03-17 13:59:29 +0530

    Add Go Routines

--- controllers/event.go
+++ controllers/event.go
@@ -11,45 +11,78 @@ import (
 func (d Db) CreateEvent(s models.Event) (string, string) {
 	var rf models.Event
 	var name string
+	var err error
+	flagch := make(chan int)
 	name = strings.ToLower(s.Name)
 	flag := strings.Split(s.Name, " ")
 	name = strings.Join(flag, "")
 	var id = name + time.Now().Month().String()
 	event := models.Event{Id: id, Name: s.Name, Fields: s.Fields, Values: make([]models.Record, 0)}
-	err := d.Session.DB("rbot").C("event").Find(bson.M{"id": id}).One(&rf)
-	if err != nil && err.Error() != "not found" {
-		log.Println(err)
-		return "Something Went Wrong", ""
+	go func() {
+		FindOne(d, "event", bson.M{"id": id}, &rf, &err)
+		flagch <- 1
+	}()
+	if <-flagch == 1 {
+		if err != nil && err.Error() != "not found" {
+			log.Println(err)
+			return "Something Went Wrong", ""
+		}
+		if rf.Id != "" {
+			return "Event Already Exists", ""
+		}
+		go func() {
+			Insert(d, "event", event, &err)
+			log.Println("TEST")
+			log.Println(err)
+			flagch <- 2
+		}()
+		for i := range flagch {
+			if i == 2 {
+				if err != nil {
+					log.Println(err)
+					return "Something Went Wrong", ""
+				}
+				return "Event Created", ""
+				break
+			}
+		}
 	}
-	if rf.Id != "" {
-		return "Event Already Exists", ""
-	}
-	err = d.Session.DB("rbot").C("event").Insert(event)
-	if err != nil {
-		log.Println(err)
-		return "Something Went Wrong", ""
-	}
-	return "Event Created", ""
+	return "", ""
 }
 
 func (d Db) InsertRecord(name string, s models.Record) string {
 	var fields models.Event
-	err := d.Session.DB("rbot").C("event").Find(bson.M{"id": name}).Select(bson.M{"fields": 1}).One(&fields)
-	if err != nil && err.Error() == "not found" {
-		return "No Such Event Exist"
+	var err error
+	flagch := make(chan int)
+	go func() {
+		FindOneProject(d, "event", bson.M{"id": name}, bson.M{"fields": 1}, &fields, &err)
+		flagch <- 1
+	}()
+	if <-flagch == 1 {
+		if err != nil && err.Error() == "not found" {
+			return "No Such Event Exist"
+		}
+		if err != nil {
+			log.Println(err)
+			return "Something Went Wrong"
+		}
+		values := make(map[string]string)
+		for _, v := range fields.Fields {
+			values[v] = s[v]
+		}
+		go func() {
+			Update(d, "event", bson.M{"id": name}, bson.M{"$push": bson.M{"values": values}}, &err)
+			flagch <- 2
+		}()
+		for i := range flagch {
+			if i == 2 {
+				if err != nil {
+					log.Println(err)
+					return "Something Went Wrong"
+				}
+				return "Record Posted Successfully"
+			}
+		}
 	}
-	if err != nil {
-		log.Println(err)
-		return "Something Went Wrong"
-	}
-	values := make(map[string]string)
-	for _, v := range fields.Fields {
-		values[v] = s[v]
-	}
-	err = d.Session.DB("rbot").C("event").Update(bson.M{"id": name}, bson.M{"$push": bson.M{"values": values}})
-	if err != nil {
-		log.Println(err)
-		return "Something Went Wrong"
-	}
-	return "Record Posted Successfully"
+	return ""
 }
---
+++ controllers/helper.go
@@ -0,0 +1,26 @@
+package funcs
+
+import (
+	"gopkg.in/mgo.v2/bson"
+	"rbot/models"
+)
+
+func Insert(d Db, c string, e models.Event, err *error) {
+	*err = nil
+	*err = d.Session.DB("rbot").C(c).Insert(e)
+}
+
+func FindOne(d Db, c string, q bson.M, rf *models.Event, err *error) {
+	*err = nil
+	*err = d.Session.DB("rbot").C(c).Find(q).One(rf)
+}
+
+func FindOneProject(d Db, c string, q bson.M, p bson.M, rf *models.Event, err *error) {
+	*err = nil
+	*err = d.Session.DB("rbot").C(c).Find(q).Select(p).One(rf)
+}
+
+func Update(d Db, c string, q bson.M, uq bson.M, err *error) {
+	*err = nil
+	*err = d.Session.DB("rbot").C(c).Update(q, uq)
+}
